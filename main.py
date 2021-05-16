c4fad271082f56762c4f572f23ec4677e61a9f48
Vlad Lytvynenko <sir.sagramor@gmail.com>
2021-05-16 12:08:00 +0300

    Added reame

--- main.py
+++ main.py
@@ -1,35 +1,7 @@
 from executor import Executor
 
-"""
-mj create
-mj create -c -n Vlad -a daily
-mj aliases
-mj get daily
-mj connect daily
-mj connect daily -n "Brother"
-mj rename daily weekly
-mj aliases
-mj get weekly
-mj alias ca91bd29-874e-46fa-be75-4f6f4cdc8a5f party
-mj aliases
-mj get party
-mj remove party
-mj get-config -p
-mj get-config
-mj set-name "Vlad Lytvynenko"
-mj connect weekly
-mj set-config example.json
-mj get-config -p
-mj aliases
-mj set-name "Vlad Lytvynenko"
-mj connect daily
-mj connect daily -n "Oleg Petrov"
-"""
-
-
 if __name__ == '__main__':
     Executor().execute_from_command_line()
 
 # TODO make this installable by pip
-# TODO make this executable and add alias with mj? Or just add 
-# TODO push to repository
+# TODO make this executable and add alias with mj
