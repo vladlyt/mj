04644f756afa994d410bfedb7767d417d2faa52b
Vlad Lytvynenko <sir.sagramor@gmail.com>
2021-05-26 23:15:06 +0300

    Fixed package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name='mj',
+   name='meetenjoy',
    version='1.0',
    description='Tool for conferences',
    license="MIT",
