44f6d15e9e08caad7a69de9a0a67aacf433b2049
Vlad Lytvynenko <sir.sagramor@gmail.com>
2021-05-26 23:16:42 +0300

    Fixed download url

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name='meetenjoy',
-   version='1.0',
-   description='Tool for conferences',
-   license="MIT",
-   long_description=long_description,
-   author='Vlad Lytvynenko',
-   author_email='sir.sagramor@gmail.com',
-   url="https://meetenjoy.herokuapp.com",
-   download_url="https://github.com/vladlytvynenko/mj/archive/refs/tags/1.0.1.tar.gz",
-   packages=['meetenjoy'],
-   install_requires=['requests'],
-   scripts=[
-            'scripts/mj',
-           ]
+    name='meetenjoy',
+    version='1.0',
+    description='Tool for conferences',
+    license="MIT",
+    long_description=long_description,
+    author='Vlad Lytvynenko',
+    author_email='sir.sagramor@gmail.com',
+    url="https://meetenjoy.herokuapp.com",
+    download_url="https://github.com/vladlytvynenko/mj/archive/refs/tags/1.0.2.tar.gz",
+    packages=['meetenjoy'],
+    install_requires=['requests'],
+    scripts=[
+        'scripts/mj',
+    ]
 )
